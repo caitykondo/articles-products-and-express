78acdf4658a0b922e1ab89a7f5db35dbbae2977c
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-01-12 18:14:29 -1000

    linked test dir with sanity check for future testing with mocha/chai/supertest

---
+++ test/server-spec.js
@@ -0,0 +1,13 @@
+const server = require('../server');
+const supertest = require('supertest');
+const chai = require('chai');
+chai.should();
+
+const agent = supertest.agent(server);
+
+describe('sanity', () => {
+
+  it('should be true', () => {
+    true.should.equal(true);
+  });
+});
