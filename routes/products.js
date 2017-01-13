b78436bdcfc702edde8ed82dc8bf0fbefca00e74
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-01-12 18:40:20 -1000

    using body-parser to handle POST requests to products/

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,8 @@
 const express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('sanity check');
+router.post('/', (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 let articles = require('./routes/articles');
@@ -7,6 +8,8 @@ let products = require('./routes/products');
 app.get('/', (req, res) => {
   res.send('hello');
 });
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/products', products);
 app.use('/articles', articles);
